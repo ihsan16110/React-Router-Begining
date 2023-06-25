912c525e82617cc40a9704d446a9055a6093e374
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2023-06-25 06:45:01 +0600

    6-25-23

---
+++ explore-routes/src/__steps.js
@@ -0,0 +1,10 @@
+/*
+1. Install react router:npm i react-router-dom
+2. Create a router use a createBrowserRouter
+3. Add RouterProvide and pass router props
+4. Create Some route
+5.
+6.
+7.
+8.
+*/
