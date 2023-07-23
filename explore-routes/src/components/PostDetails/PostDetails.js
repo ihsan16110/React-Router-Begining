478546e1d96f476385d0ca6a7df1de5c6c94e525
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2023-07-23 11:59:05 +0600

    isActive

---
+++ explore-routes/src/components/PostDetails/PostDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostDetails = () => {
+    return (
+        <div>
+            <h2>Details about Post</h2>
+            
+      
+        </div>
+    );
+};
+
+export default PostDetails;
+
